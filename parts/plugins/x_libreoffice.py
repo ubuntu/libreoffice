aa82d6f7d837a356e40d4c31420cba1be001048e
Bjoern Michaelsen <bjoern.michaelsen@canonical.com>
2016-09-12 16:30:51 +0200

    bump to 5.2.0.4

--- parts/plugins/x_libreoffice.py
+++ parts/plugins/x_libreoffice.py
@@ -40,7 +40,7 @@ CONFFLAGS = [
 	'--enable-scripting-beanshell',
 	'--enable-scripting-javascript',
 	'--with-alloc=system',
-	'--with-build-version=libreoffice-5.2.0.3-snap1',
+	'--with-build-version=libreoffice-5.2.0.4-snap1',
 	'--with-gdrive-client-id=$(GOOGLEAPI_CLIENTID_UBUNTU)',
 	'--with-gdrive-client-secret=$(GOOGLEAPI_CLIENTSECRET_UBUNTU)',
 	'--with-system-libexttextcat',
@@ -167,7 +167,7 @@ class LibreOfficePlugin(autotools.AutotoolsPlugin):
         self.run(['git', 'clone',
             '--depth=1',
             '-v',
-            '--branch=libreoffice-5.2.0.3',
+            '--branch=libreoffice-5.2.0.4',
             '/home/bjoern/checkouts/libreoffice',
             os.path.join(self.builddir, 'build')])
         self.run(
