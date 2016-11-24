6e2fc428de92e12ed2e6841c0e02a4aadb2f0806
Bjoern Michaelsen <bjoern.michaelsen@canonical.com>
2016-11-24 13:16:08 +0100

    bump to 5.3.0 beta1

--- parts/plugins/x_libreoffice.py
+++ parts/plugins/x_libreoffice.py
@@ -40,7 +40,7 @@ CONFFLAGS = [
 	'--enable-scripting-beanshell',
 	'--enable-scripting-javascript',
 	'--with-alloc=system',
-	'--with-build-version=libreoffice-5.2.3.2-snap1',
+	'--with-build-version=libreoffice-5.3.0.0.beta1-snap1',
 	'--with-gdrive-client-id=$(GOOGLEAPI_CLIENTID_UBUNTU)',
 	'--with-gdrive-client-secret=$(GOOGLEAPI_CLIENTSECRET_UBUNTU)',
 	'--with-system-libexttextcat',
@@ -168,7 +168,7 @@ class LibreOfficePlugin(autotools.AutotoolsPlugin):
         self.run(['git', 'clone',
             '--depth=1',
             '-v',
-            '--branch=libreoffice-5.2.3.2',
+            '--branch=libreoffice-5.3.0.0.beta1',
             'https://github.com/LibreOffice/core.git',
             os.path.join(self.builddir, 'build')])
         self.run(
