c162cb7e92970cb381c8c0954942d7889a0e3cc7
Olivier Tilloy <olivier.tilloy@canonical.com>
2018-02-19 22:24:34 +0100

    Add a script that generates an initial java configuration file (LP: #1750111)

---
+++ javasettings.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import base64
+import os
+import os.path
+import re
+import subprocess
+import sys
+
+
+if __name__ == '__main__':
+    snap = os.environ["SNAP"]
+    snap_arch = os.environ["SNAP_ARCH"]
+    snap_user_data = os.environ["SNAP_USER_DATA"]
+    lo_config_dir = os.path.join(snap_user_data, ".config", "libreoffice",
+                                 "4", "user", "config")
+    javasettings_arch = {"i386": "x86", "amd64": "X86_64"}.get(snap_arch)
+    javasettings_filename = \
+        "javasettings_Linux_{}.xml".format(javasettings_arch)
+    javasettings_filepath = os.path.join(lo_config_dir, javasettings_filename)
+    if os.path.isfile(javasettings_filepath):
+        sys.exit()
+
+    if not os.path.exists(lo_config_dir):
+        os.makedirs(lo_config_dir)
+
+    jre = "{}/usr/lib/jvm/java-8-openjdk-{}/jre".format(snap, snap_arch)
+    jvm = "{}/lib/{}".format(jre, snap_arch)
+
+    java_location = "file://{}".format(jre)
+
+    java_output = subprocess.getoutput("{}/bin/java -version".format(jre))
+    java_version = re.match('openjdk version "(?P<version>[\d\._]+)"',
+                            java_output).group("version")
+
+    vendor_data = []
+    vendor_data.append("file://{}/server/libjvm.so".format(jvm))
+    paths = []
+    paths.append("{}/client".format(jvm))
+    paths.append("{}/server".format(jvm))
+    paths.append("{}/native_threads".format(jvm))
+    paths.append(jvm)
+    vendor_data.append(":".join(paths))
+    vendor_data.append("")
+    vendor_data = "\n".join(vendor_data)
+    vendor_data = vendor_data.encode("utf-16")[2:]  # remove BOM
+    vendor_data = base64.b16encode(vendor_data).decode()
+
+    template = """<?xml version="1.0" encoding="UTF-8"?>
+<java xmlns="http://openoffice.org/2004/java/framework/1.0"
+      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+  <javaInfo xsi:nil="false" vendorUpdate="2013-05-02" autoSelect="false">
+    <vendor>Oracle Corporation</vendor>
+    <location>{javaLocation}</location>
+    <version>{javaVersion}</version>
+    <features>0</features>
+    <requirements>1</requirements>
+    <vendorData>{vendorData}</vendorData>
+  </javaInfo>
+  <enabled xsi:nil="true"/>
+</java>"""
+    javasettings = template.format(javaLocation=java_location,
+                                   javaVersion=java_version,
+                                   vendorData=vendor_data)
+    with open(javasettings_filepath, "w") as f:
+        f.write(javasettings)
